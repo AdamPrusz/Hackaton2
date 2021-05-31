e856da683de689211d6ed023aba6a31ec01986b2
AdamPrusz <AdamPrusz@github.com>
2021-06-01 00:15:29 +0200

    conditions

--- stacja-projekt.py
+++ stacja-projekt.py
@@ -1,61 +1,44 @@
-#'%Y-%m-%d %H:%M:%S'
-
-#sprawdenioe smogu i temperatury dla wybranych lokalizacji
-
-
-import requests #do Api
-import json #przeszktałcenie do json
-from datetime import timedelta, datetime #do dat
-import matplotlib.pyplot as plt #rysowanie wykresów
-from PROJEKT.PROJEKT.Project import geo
-
-
-# i kraj, ale tez tylko miasto,a i tak
-#zamieni to na dlugosc i szerokosc geograficzna, ktora potrzebna jest do zebrania danych. zamieniam to, bo w API dla
-#historycznych danych podajemy dlugosc i szerokos geog.
+import requests
+import json
+from datetime import timedelta, datetime
+import matplotlib.pyplot as plt
+import geolocation_module as geo
+import format_module as format
 
 
 def menu():
-    #Użytkownik wybiera jakie dane chce sprawdzić
-    print("Welcome \n 1. Check PM 2.5 and PM 10 for the last 24hours \n 2. Check temperature for the last 24 hours")
-    choice = input("Gimme the number: ")
-
-    if choice == '1':
-        smog()
-
-    if choice == '2':
-        temperature()
+    print("********************************** \n Welcome to the weather history! \n ********************************** \n"
+          " 1. Check PM 2.5 and PM 10 for the last 24 hours \n 2. Check temperature for the last 24 hours \n "
+          "**********************************")
 
 
 def plotting_smog(x, y1, y2):
-    #rysowanie wykresu dla dwóch parametrow, np pm25 i pm10, temp i cisnienie
     plt.plot(x, y1, label="PM 2,5")
     plt.plot(x, y2, label="PM 10")
     plt.xlabel('Hours')
     plt.ylabel('μg/m3')
-    plt.title('The graph of PM25 and PM10 in chosen location for the last 24 hours')
+    plt.title('The graph of PM2,5 and PM10 in chosen location for the last 24 hours')
     plt.legend()
     plt.savefig('smog.pdf')
     plt.show()
 
 
 def plotting_temp(x,y):
-    #wykres tylko dla jednego paramateru np temperatura godzinowo, wykres słupkowy
     plt.bar(x, y)
     plt.xlabel('Temperature')
-    plt.ylabel('Celcius degrees')
+    plt.ylabel('Degrees Celsius')
     plt.title('The graph of temperature in chosen location for the last 24 hours')
     plt.savefig('temperature.pdf')
     plt.show()
 
+
 def smog():
-    #metoda ktora wyciaga PM25 i Pm10 z API
-    timeBefore = timedelta(days=1) #o ile chcemy sie cofnac days=1 bo 24h
+    timeBefore = timedelta(days=1)
     searchDate = datetime.today() - timeBefore
-    user = input("Give me street, city or country name --> ")
+    user = input("Give the adress to be checked (at least city, but you can also add street and country) ---> ")
     parameters = {
-        'lat': geo.geo_lat(user),
-        'lon': geo.geo_lon(user),
+        'lat': geo.latitude(user),
+        'lon': geo.longtitude(user),
         'appid': "0fb8ce688518ffd7d1192443399712d7",
         'start': int(searchDate.timestamp()),
         'end': int(datetime.today().timestamp())
@@ -78,21 +61,18 @@ def smog():
 
     plotting_smog(days, pm25_values, pm10_values)
 
+
 def temperature():
-    #metoda wyciagajace temperature z API
     timeBefore = timedelta(days=1)
     searchDate = datetime.today() - timeBefore
-
-    user = input("Give me street, city or country name --> ")
-
+    user = input("Give the adress to be checked (at least city, but you can also add street and country) ---> ")
     parameters = {
-        'lat': geo.geo_lat(user),
-        'lon': geo.geo_lon(user),
+        'lat': geo.latitude(user),
+        'lon': geo.longtitude(user),
         'dt': int(searchDate.timestamp()),
         'appid': "0fb8ce688518ffd7d1192443399712d7",
         'units': "metric"
     }
-
     r = requests.get("https://api.openweathermap.org/data/2.5/onecall", parameters)
     try:
         content = r.json()
@@ -100,15 +80,34 @@ def temperature():
         print("Niepoprawny format")
 
     list_elements = content['hourly']
-
     days = []
     temperature = []
-
     for step in list_elements:
         days.append(datetime.utcfromtimestamp(step['dt']).strftime('%H:%M'))
         temperature.append(step['temp'])
 
     plotting_temp(days, temperature)
 
-menu()
+
+if __name__ == '__main__':
+    menu()
+    while True:
+        choice = input("Please input the number: ")
+        if choice == "1":
+            smog()
+            user = input("Would you like to check something more? Type yes or no: ")
+            if user == "yes":
+                continue
+            if user == "no":
+                break
+        if choice == '2':
+            temperature()
+            user = input("Would you like to check something more? Type yes or no: ")
+            if user == "yes":
+                continue
+            if user == "no":
+                break
+        else:
+            print("Something went wrong, please try again")
+
 
