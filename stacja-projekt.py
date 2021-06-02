a62e9ed7bd022eedc9b3593a42cd19abf53ac7b8
AdamPrusz <AdamPrusz@github.com>
2021-06-02 13:25:02 +0200

    create classes

--- stacja-projekt.py
+++ stacja-projekt.py
@@ -7,40 +7,51 @@ import format_module as format
 
 
 def menu():
-    print('Welcome to Weather application using OpenWeatherMap\'s API! \n')
+    print('Welcome to Weather application using OpenWeatherMap\'s API! \n \n'
+          'What would you like to check? \n 1. Smog condition (values of PM2.5 and PM10)for the last 24 hours \n '
+          '2. Temperature for the last 24 hours \n 3. Weather forecast for the 5 days \n')
 
 
 def main():
     while True:
-        choice = input("Please input 1, 2 or 3: ")
-        if choice == "1":
-            smog()
-            user = input("Would you like to check something else? Type yes or no: ")
-            if user == "yes":
+        user = input("Please input 1, 2 or 3: ")
+        if user == "1":
+            http = 'http://api.openweathermap.org/data/2.5/air_pollution/history'
+            station = Smog_station(http)
+            station.smog_values(station.smog())
+            question = input("Would you like to check something else? Type yes or no: ")
+            if question == "yes":
                 continue
-            if user == "no":
+            if question == "no":
+                print("Thank you for using our application. Have a ncie day!")
                 break
             else:
                 print("Something went wrong, bye")
                 break
 
-        if choice == '2':
-            temperature()
-            user = input("Would you like to check something more? Type yes or no: ")
-            if user == "yes":
+        if user == '2':
+            http = 'https://api.openweathermap.org/data/2.5/onecall'
+            station = Temp_station(http)
+            station.temperature_values(station.temperature())
+            question = input("Would you like to check something more? Type yes or no: ")
+            if question == "yes":
                 continue
-            if user == "no":
+            if question == "no":
+                print("Thank you for using our application. Have a ncie day!")
                 break
             else:
                 print("Something went wrong, bye")
                 break
 
-        if choice == '3':
-            max_temperature()
-            user = input("Would you like to check something more? Type yes or no: ")
-            if user == "yes":
+        if user == '3':
+            http = 'https://api.openweathermap.org/data/2.5/forecast'
+            station = Forecast_station(http)
+            station.forecast_values(station.forecast())
+            question = input("Would you like to check something more? Type yes or no: ")
+            if question == "yes":
                 continue
-            if user == "no":
+            if question == "no":
+                print("Thank you for using our application. Have a ncie day!")
                 break
             else:
                 print("Something went wrong, bye")
@@ -82,91 +93,90 @@ def plotting_forecast(x, y):
     plt.show()
 
 
-def smog():
-    timeBefore = timedelta(days=1)
-    searchDate = datetime.today() - timeBefore
-    user = input("Give the adress to be checked (at least city, but you can also add street and country) ---> ")
-    parameters = {
-        'lat': geo.latitude(user),
-        'lon': geo.longtitude(user),
-        'appid': "0fb8ce688518ffd7d1192443399712d7",
-        'start': int(searchDate.timestamp()),
-        'end': int(datetime.today().timestamp())
-    }
-    r = requests.get("http://api.openweathermap.org/data/2.5/air_pollution/history", parameters)
-    try:
-        content = r.json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format")
-
-    list_elements = content['list']
-    days = []
-    pm25_values = []
-    pm10_values = []
-    for step in list_elements:
-        days.append(datetime.utcfromtimestamp(step['dt']).strftime('%H:%M'))
-        components = step['components']
-        pm25_values.append(components['pm2_5'])
-        pm10_values.append(components['pm10'])
-
-    plotting_smog(days, pm25_values, pm10_values)
+class Smog_station:
+    def __init__(self, http, timeBefore=timedelta(days=1), units='metric'):
+        self.http = http
+        self.timeBefore = timeBefore
+        self.units = units
+        self.timeToday = datetime.today()
+        self.appid = '0fb8ce688518ffd7d1192443399712d7'
+        self.user = input("Please write location you want to check -->  ")
 
 
-def temperature():
-    timeBefore = timedelta(days=1)
-    searchDate = datetime.today() - timeBefore
-    user = input("Give the adress to be checked (at least city, but you can also add street and country) ---> ")
-    parameters = {
-        'lat': geo.latitude(user),
-        'lon': geo.longtitude(user),
-        'dt': int(searchDate.timestamp()),
-        'appid': "0fb8ce688518ffd7d1192443399712d7",
-        'units': "metric"
-    }
-    r = requests.get("https://api.openweathermap.org/data/2.5/onecall", parameters)
-    try:
-        content = r.json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format")
+    def smog(self):
+        searchDate = self.timeToday - self.timeBefore
+        parameters = {
+            'lat': geo.latitude(self.user),
+            'lon': geo.longtitude(self.user),
+            'appid': self.appid,
+            'start': int(searchDate.timestamp()),
+            'end': int(self.timeToday.timestamp()),
+            'dt': int(searchDate.timestamp()),
+            'units': self.units
+        }
+        r = requests.get(self.http, parameters)
+        try:
+            content = r.json()
+        except json.decoder.JSONDecodeError:
+            print("Niepoprawny format")
 
-    list_elements = content['hourly']
-    days = []
-    temperature = []
-    for step in list_elements:
-        days.append(datetime.utcfromtimestamp(step['dt']).strftime('%H:%M'))
-        temperature.append(step['temp'])
+        return content
 
-    plotting_temp(days, temperature)
+    def smog_values(self, content):
+        list_elements = content['list']
+        days = []
+        pm25_values = []
+        pm10_values = []
+        for step in list_elements:
+            days.append(datetime.utcfromtimestamp(step['dt']).strftime('%H:%M'))
+            components = step['components']
+            pm25_values.append(components['pm2_5'])
+            pm10_values.append(components['pm10'])
 
-def max_temperature():
-    user = input("Give the adress to be checked ---> ")
-    parameters = {
-        'lat': geo.latitude(user),
-        'lon': geo.longtitude(user),
-        'appid': "0fb8ce688518ffd7d1192443399712d7",
-        'units': "metric",
-    }
-    r = requests.get("https://api.openweathermap.org/data/2.5/forecast", parameters)
-    try:
-        content = r.json()
-    except json.decoder.JSONDecodeError:
-        print("Niepoprawny format")
+        plotting_smog(days, pm25_values, pm10_values)
 
-    list_elements = content["list"]
-    days = []
-    temp_max = []
-    for step in list_elements:
-        days.append(datetime.utcfromtimestamp(step['dt']).strftime('%d/%m %H:%M'))
-        main = step['main']
-        temp_max.append(main['temp_max'])
 
-    plotting_forecast(days, temp_max)
+class Temp_station(Smog_station):
+    def __init__(self, http):
+        super().__init__(http)
+
+    def temperature(self):
+        return super().smog()
+
+    def temperature_values(self, content):
+        list_elements = content['hourly']
+        days = []
+        temperature = []
+        for step in list_elements:
+            days.append(datetime.utcfromtimestamp(step['dt']).strftime('%H:%M'))
+            temperature.append(step['temp'])
+
+        plotting_temp(days, temperature)
+
+
+class Forecast_station(Smog_station):
+    def __init__(self, http):
+        super().__init__(http)
+
+    def forecast(self):
+        return super().smog()
+
+    def forecast_values(self, content):
+        list_elements = content["list"]
+        days = []
+        temp_max = []
+        for step in list_elements:
+            days.append(datetime.utcfromtimestamp(step['dt']).strftime('%d/%m %H:%M'))
+            main = step['main']
+            temp_max.append(main['temp_max'])
+
+        plotting_forecast(days, temp_max)
+
 
 #----- mian code --------------------------------------
+
 if __name__ == '__main__':
     menu()
-    print('What would you like to check? \n 1. Smog condition (PM2.5 and PM10 for the last 24 hours \n '
-          '2. Temperature for the last 24 hours \n 3. Weather forecast for the 5 days \n')
     main()
 
 
